bbd9141cd26fb974ca8f518eae6cc0efea6074f8
Kasey Labelle <kasey.l.labelle@gmail.com>
2018-02-06 18:06:55 -0500

    updates

---
+++ routes/signUpRouter.js
@@ -0,0 +1,6 @@
+// use router when you have multiple different actions happening from one url
+
+// signing up as shelter or adopter?
+// route to:
+//   /signup/shelter
+//   /signup/adopter
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/public/views/index.html');
 });
 
 let server;
